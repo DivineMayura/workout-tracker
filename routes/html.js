3019b1649947cd31680ac333dcc1b71693718664
May <MayyFaucher@gmail.com>
2021-11-15 21:33:26 -0800

    added mostly complete html routes

--- routes/html.js
+++ routes/html.js
@@ -1,12 +1,29 @@
-const { router } = require("express");
+const router = require("express").router();
 // const db = require("./models");
 
+router.get("/exercise", async (req, res) => {
+    try{
+        res.send("../public/exercise.html")
+    } catch (err) {
+        res.json(err)
+    }
+})
 
+router.get("/stats", async (req, res) => {
+    try{
+        res.send("../public/stats.html")
+    } catch (err) {
+        res.json(err)
+    }
+})
 
-
-
-
-
+router.get("/index", async (req, res) => {
+    try{
+        res.send("../public/index.html")
+    } catch (err) {
+        res.json(err)
+    }
+})
 
 
 module.exports = router;
